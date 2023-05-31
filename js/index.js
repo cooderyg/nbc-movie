65c8d1f5b11e10119241df09a3e1e6a23d9c85c2
cooderyg <ygk9604@naver.com>
2023-05-31 21:51:47 +0900

    무한스크롤 구현

--- js/index.js
+++ js/index.js
@@ -12,15 +12,65 @@ import template from "./template.js";
     const data = await res.json()
     let rows = data.results;
 
-    //template.js로 템플릿 제작 순회  
+    // template.js로 템플릿 제작 순회  
     let temp = template(rows)
 
-    //배열 정리후 데이터바인딩
+    // 배열 정리후 데이터바인딩
     let joinTemp = temp.join("");
     const nowPlayEl = document.querySelector("#now-play");
     nowPlayEl.innerHTML = joinTemp;
 
-    //li삽입 후 선택자 할당
+    // 마지막 li 찾기
+    let LastLi = document.querySelector("#now-play li:last-child");
+    let pageCount = 1;
+    
+    // 무한스크롤 기능구현
+    // 요소의 가시성관찰
+    const io = new IntersectionObserver((entries, observer) =>{
+      entries.forEach(function(entry){
+        if(entry.isIntersecting){
+          pageCount++
+          const  ioFetch = async () =>{
+            const res = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=1609961e6087bc908a47717d3912b94c&page=${pageCount}&language=ko-KR`)
+            const data = await res.json()
+            let rows = data.results;
+        
+            // template.js로 템플릿 제작 순회  
+            let temp = template(rows)
+            
+            // 배열 정리후 데이터바인딩
+            let joinTemp = temp.join("");
+            const nowPlayEl = document.querySelector("#now-play");
+            nowPlayEl.insertAdjacentHTML("beforeend", joinTemp)
+            
+            // 모달이벤트에 넣을 list 재할당
+            movieLis = document.querySelectorAll("#now-play li");
+            
+            // 모달 클릭 데이터삽입 함수
+            clickLi(rows, movieLis, modalEl, modalContentEl);
+
+            // 기존에 사용한 마지막 li 관찰 종료
+            io.unobserve(LastLi);
+
+            // 템플릿 추가후 바뀐 마지막 li 재할당 
+            LastLi = document.querySelector("#now-play li:last-child");
+            console.log(LastLi)
+
+            // 새로운 마지막 li에 가시성관찰
+            io.observe(LastLi)
+          }
+          // 무한스크롤 패칭
+          ioFetch()
+        }
+      })
+    })
+    console.log(LastLi)
+
+    //첫 번째 관찰 시작
+    io.observe(LastLi)
+
+
+    //모달이벤트에 넣을 list
     let movieLis = document.querySelectorAll("#now-play li");
 
     //모달 선택자
@@ -44,6 +94,7 @@ import template from "./template.js";
 
       // input value가져오기
       let searchValue = searchInputEl.value;
+      if(searchValue === "") return alert("검색어를 작성해주세요")
 
       // ver.1 처음 받아온 데이터 필터형식
       // const copyRows = [...rows];
@@ -76,8 +127,6 @@ import template from "./template.js";
       if (e.keyCode === 13) search();
     });
   }
-
   fetchMovie();
   
-
-        
+  
